f77093103f3a94d78228f685fe9d1ff4e1488f90
Eugene Revutskiy <revytskiy.evgen@gmail.com>
2020-11-27 18:16:22 +0200

    Updates

---
+++ precache-manifest.8ba6c3314932eeafbc04d084c865b21f.js
@@ -0,0 +1,138 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d334bfcb636d93086277705a6a573654",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "807a2842bcd3a85a7f3a",
+    "url": "/portfolio/static/css/2.700d419c.chunk.css"
+  },
+  {
+    "revision": "10091e533532b314cccb",
+    "url": "/portfolio/static/css/main.a6df7eee.chunk.css"
+  },
+  {
+    "revision": "807a2842bcd3a85a7f3a",
+    "url": "/portfolio/static/js/2.3bf308e8.chunk.js"
+  },
+  {
+    "revision": "001e3af8aadb8024472157c9f539d5d7",
+    "url": "/portfolio/static/js/2.3bf308e8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "10091e533532b314cccb",
+    "url": "/portfolio/static/js/main.0151319b.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "f7940bd9d38847d29803b2b2fac882b5",
+    "url": "/portfolio/static/media/atl.f7940bd9.png"
+  },
+  {
+    "revision": "23348b3b6730c2991377465501054ca7",
+    "url": "/portfolio/static/media/dnj_studio.23348b3b.png"
+  },
+  {
+    "revision": "085b1dd8427dbeff10bd55410915a3f6",
+    "url": "/portfolio/static/media/fa-brands-400.085b1dd8.ttf"
+  },
+  {
+    "revision": "0fabb6606be4c45acfeedd115d0caca4",
+    "url": "/portfolio/static/media/fa-brands-400.0fabb660.eot"
+  },
+  {
+    "revision": "cac68c831145804808381a7032fdc7c2",
+    "url": "/portfolio/static/media/fa-brands-400.cac68c83.woff2"
+  },
+  {
+    "revision": "ccfdb9dc442be0c629d331e94497428b",
+    "url": "/portfolio/static/media/fa-brands-400.ccfdb9dc.svg"
+  },
+  {
+    "revision": "dc0bd022735ed218df547742a1b2f172",
+    "url": "/portfolio/static/media/fa-brands-400.dc0bd022.woff"
+  },
+  {
+    "revision": "05b53beb21e3ef13d28244545977152d",
+    "url": "/portfolio/static/media/fa-regular-400.05b53beb.woff"
+  },
+  {
+    "revision": "1a78af4105d4d56e6c34f76dc70bf1bc",
+    "url": "/portfolio/static/media/fa-regular-400.1a78af41.ttf"
+  },
+  {
+    "revision": "3a3398a6ef60fc64eacf45665958342e",
+    "url": "/portfolio/static/media/fa-regular-400.3a3398a6.woff2"
+  },
+  {
+    "revision": "ad3a7c0d77e09602f4ab73db3660ffd8",
+    "url": "/portfolio/static/media/fa-regular-400.ad3a7c0d.eot"
+  },
+  {
+    "revision": "e75dfd904d366a2560c63c23cfc98ef8",
+    "url": "/portfolio/static/media/fa-regular-400.e75dfd90.svg"
+  },
+  {
+    "revision": "03ba7cb710104df27f1c9c46d64bee4e",
+    "url": "/portfolio/static/media/fa-solid-900.03ba7cb7.svg"
+  },
+  {
+    "revision": "781e85bb50c8e8301c30de56b31b1f04",
+    "url": "/portfolio/static/media/fa-solid-900.781e85bb.ttf"
+  },
+  {
+    "revision": "89bd2e38475e441a5cd70f663f921d61",
+    "url": "/portfolio/static/media/fa-solid-900.89bd2e38.eot"
+  },
+  {
+    "revision": "c500da19d776384ba69573ae6fe274e7",
+    "url": "/portfolio/static/media/fa-solid-900.c500da19.woff2"
+  },
+  {
+    "revision": "ee09ad7553b8ad3d81150d609d5341a0",
+    "url": "/portfolio/static/media/fa-solid-900.ee09ad75.woff"
+  },
+  {
+    "revision": "e970608c8e8be326a66e0e4c177759ce",
+    "url": "/portfolio/static/media/galinas.e970608c.png"
+  },
+  {
+    "revision": "1b41be93224d785fe77cfbd17d753643",
+    "url": "/portfolio/static/media/main-bg.1b41be93.jpg"
+  },
+  {
+    "revision": "1ddd80f5e4546c12cf68059f0224c024",
+    "url": "/portfolio/static/media/mas.1ddd80f5.jpeg"
+  },
+  {
+    "revision": "1ccc8a3b7f8daba9bf2543f0f25dc445",
+    "url": "/portfolio/static/media/me.1ccc8a3b.jpg"
+  },
+  {
+    "revision": "556510c3c119ff2ecb4e9c95e727c1fc",
+    "url": "/portfolio/static/media/mon_pion.556510c3.png"
+  },
+  {
+    "revision": "057ce2ea92b46a5505ab47986dc85158",
+    "url": "/portfolio/static/media/resume-bg.057ce2ea.jpg"
+  },
+  {
+    "revision": "1cefcc6846dd39bb29310f0d62a2f94d",
+    "url": "/portfolio/static/media/rsub.1cefcc68.jpeg"
+  },
+  {
+    "revision": "62e68afc6ec3b78f6183725057956b28",
+    "url": "/portfolio/static/media/skills-bg.62e68afc.jpg"
+  },
+  {
+    "revision": "7f4e9263d9b6ef750b530f007741a8f1",
+    "url": "/portfolio/static/media/skills-main-bg.7f4e9263.jpeg"
+  },
+  {
+    "revision": "3a3461a0662039efb2541556c57ca813",
+    "url": "/portfolio/static/media/trumba_trans.3a3461a0.png"
+  }
+]);
